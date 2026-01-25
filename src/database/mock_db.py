9a7bdd40ea73e5b30ddc87c2be9f7a81087341c8
eyaChikhaoui <eyachikhaoui2003@gmail.com>
2026-01-25 14:42:57 +0100

    Restoring project structure and code from local backup

---
+++ src/database/mock_db.py
@@ -0,0 +1,14 @@
+# TO-DO: Person 2 (Backend Lead) will write the code here
+
+def search_similar(vector):
+    """
+    P2: Write your Qdrant search logic here.
+    Input: Vector (list of floats)
+    Output: List of dictionaries (protein names & scores)
+    """
+    # حالياً، هذا كود مؤقت
+    # P2: امسح هذا واكتب كود الاتصال بـ Qdrant الحقيقي
+    return [
+        {"name": "Protein X (Demo)", "similarity": 0.99, "id": "P001"},
+        {"name": "Protein Y (Demo)", "similarity": 0.85, "id": "P002"}
+    ]
---
+++ src/encoders/mock_encoder.py
@@ -0,0 +1,12 @@
+# TO-DO: Person 1 (ML Lead) will write the code here
+import numpy as np
+
+def encode_dna(sequence):
+    """
+    P1: Write your DNABERT/Transformer logic here.
+    Input: DNA sequence (string)
+    Output: Vector embedding (list of floats)
+    """
+    # حالياً، هذا كود مؤقت لكي يعمل السيرفر
+    # P1: امسح هذا السطر واكتب كودك الحقيقي غداً
+    return np.random.rand(768).tolist()
